0725798d8637043145a1663e0f8e7f2a3fccbe9b
chenp <chenpeleg@gmail.com>
2023-01-25 19:27:25 +0200

    added prettierrc.txt

--- tests/tests.mjs
+++ tests/tests.mjs
@@ -1,7 +1,7 @@
-import test from 'node:test';
-import assert from 'node:assert/strict';
-const ping = () => 'pong';
+import test from "node:test";
+import assert from "node:assert/strict";
+const ping = () => "pong";
 
-test('ping', (_t) => {
-    assert.equal(ping(), 'pong');
+test("ping", (_t) => {
+  assert.equal(ping(), "pong");
 });
