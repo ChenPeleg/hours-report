df27b018ad2faf18ce915c0236e81ecec51d2e88
chenp <chenpeleg@gmail.com>
2023-01-25 20:41:11 +0200

    added tsconfig.json

---
+++ src/index.js
@@ -0,0 +1,10 @@
+//
+//
+//
+// xec(`cd ${dir} && git log ${authors.map(author => `--author="${author}"`).join(" ")} --pretty="%an <%ae> %ct"`, function (err, data) {
+//     if (err) {
+//         console.log(err)
+//         return
+//     }
+//
+//     var log = data.split('\n')}
