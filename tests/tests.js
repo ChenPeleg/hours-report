88157a4774b0d3a9ef3ef3c416ab2162ad8af2e8
chenp <chenpeleg@gmail.com>
2023-01-25 19:22:00 +0200

    added first test

--- tests/tests.js
+++
@@ -1,8 +0,0 @@
-
-import test from 'node:test';
-import assert from 'node:assert';
-
-test('synchronous passing test', (t) => {
-    // This test passes because it does not throw an exception.
-    assert.strictEqual(1, 1);
-});
---
+++ tests/tests.mjs
@@ -0,0 +1,7 @@
+import test from 'node:test';
+import assert from 'node:assert/strict';
+const ping = () => 'pong';
+
+test('ping', (_t) => {
+    assert.equal(ping(), 'pong');
+});
