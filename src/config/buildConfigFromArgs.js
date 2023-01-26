88eb9e5af50d8f04dbf3a52e71deae0f090a6804
chenp <chenpeleg@gmail.com>
2023-01-26 11:32:11 +0200

    finished with the buildConfigFromArgs.js

--- src/config/buildConfigFromArgs.js
+++ src/config/buildConfigFromArgs.js
@@ -5,50 +5,50 @@
  * @return { null | {data : string, jumpNextArg: boolean}}
  */
 const getArgumentFromArgs = (argument, nextArg, option) => {
-
   if (
-      argument.startsWith(`-${option.alias}`) ||
-      argument.startsWith(`--${option.name}`)
+    argument.startsWith(`-${option.alias}`) ||
+    argument.startsWith(`--${option.name}`)
   ) {
     const isAlias = argument.startsWith(`-${option.alias}`);
     const hasEqual = argument.includes('=');
     if (hasEqual) {
       const data = isAlias
-          ? argument.replace(`-${option.alias}=`)
-          : argument.replace(`--${option.name}=`);
+        ? argument.replace(`-${option.alias}=`)
+        : argument.replace(`--${option.name}=`);
       return {
-        data, jumpNextArg: false
-      }
+        data,
+        jumpNextArg: false,
+      };
     } else {
       return {
-        data: nextArg, jumpNextArg: true
-      }
-
+        data: nextArg,
+        jumpNextArg: true,
+      };
     }
-
   }
-  return null
-
-}
-
+  return null;
+};
 
 /**
- * @param {any}args
+ * @param {any} args
  * @param {CommandLineOption[]} ArgsOption
  */
 export const buildConfigFromArgs = (args, ArgsOption) => {
-  /**
-   * @type {Partial<ReportConfigurations>}
-   */
+  /** @type {Partial<ReportConfigurations>}  */
   let dataFromArgs = {};
-  for (let i = 0; i < args; i += 1) {
-
+  argsLoop: for (let i = 0; i < args; i += 1) {
     for (let option of ArgsOption) {
-      if (getArgumentFromArgs(args[i], args[i + 1], option)) {
-
+      const resArgument = getArgumentFromArgs(args[i], args[i + 1], option);
+      if (resArgument) {
+        dataFromArgs[option.configOption] = resArgument.data;
+        if (resArgument.jumpNextArg) {
+          i++;
+          continue argsLoop;
+        } else {
+          break;
+        }
       }
     }
-
   }
   return dataFromArgs;
 };
