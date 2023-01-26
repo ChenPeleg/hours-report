5d1b5b884de7c5fda19c49f1db24c59d33aabb8c
chenp <chenpeleg@gmail.com>
2023-01-26 11:10:21 +0200

    refactored the function to buildConfigFromArgs.js

--- src/config/buildConfigFromArgs.js
+++ src/config/buildConfigFromArgs.js
@@ -1,4 +1,38 @@
 /**
+ * @param {string}argument
+ * @param {string}nextArg
+ * @param {CommandLineOption } option
+ * @return { null | {data : string, jumpNextArg: boolean}}
+ */
+const getArgumentFromArgs = (argument, nextArg, option) => {
+
+  if (
+      argument.startsWith(`-${option.alias}`) ||
+      argument.startsWith(`--${option.name}`)
+  ) {
+    const isAlias = argument.startsWith(`-${option.alias}`);
+    const hasEqual = argument.includes('=');
+    if (hasEqual) {
+      const data = isAlias
+          ? argument.replace(`-${option.alias}=`)
+          : argument.replace(`--${option.name}=`);
+      return {
+        data, jumpNextArg: false
+      }
+    } else {
+      return {
+        data: nextArg, jumpNextArg: true
+      }
+
+    }
+
+  }
+  return null
+
+}
+
+
+/**
  * @param {any}args
  * @param {CommandLineOption[]} ArgsOption
  */
@@ -7,22 +41,29 @@ export const buildConfigFromArgs = (args, ArgsOption) => {
    * @type {Partial<ReportConfigurations>}
    */
   let dataFromArgs = {};
-  for (let i = 0; i < args; i += 1) {
-    let argument = args[i];
-    ArgsOption.forEach((option) => {
-      if (
-        argument.startsWith(`-${option.alias}`) ||
-        argument.startsWith(`--${option.name}`)
-      ) {
-        const isAlias = argument.startsWith(`-${option.alias}`);
-        const hasEqual = argument.includes('=');
-        if (hasEqual) {
-          dataFromArgs[option.configOption] = isAlias
-            ? argument.replace(`-${option.alias}=`)
-            : argument.replace(`--${option.name}=`);
-        }
+  argsLoop:
+      for (let i = 0; i < args; i += 1) {
+        let argument = args[i];
+
+        ArgsOption.forEach((option) => {
+          if (
+              argument.startsWith(`-${option.alias}`) ||
+              argument.startsWith(`--${option.name}`)
+          ) {
+            const isAlias = argument.startsWith(`-${option.alias}`);
+            const hasEqual = argument.includes('=');
+            if (hasEqual) {
+              dataFromArgs[option.configOption] = isAlias
+                  ? argument.replace(`-${option.alias}=`)
+                  : argument.replace(`--${option.name}=`);
+            } else {
+              dataFromArgs[option.configOption] = args[i];
+              i++;
+
+            }
+            continue argsLoop;
+          }
+        });
       }
-    });
-  }
   return dataFromArgs;
 };
