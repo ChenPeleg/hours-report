62ccc8a5ac6e37965d7834fcbbc81d53696636e7
chenp <chenpeleg@gmail.com>
2023-01-26 10:47:50 +0200

    added command line args 4

--- src/config/buildConfigFromArgs.js
+++ src/config/buildConfigFromArgs.js
@@ -1,25 +1,28 @@
-import {ArgsOption} from './argsOption.js';
-
-
 /**
  * @param {any}args
  * @param {CommandLineOption[]} ArgsOption
  */
 export const buildConfigFromArgs = (args, ArgsOption) => {
-
+  /**
+   * @type {Partial<ReportConfigurations>}
+   */
+  let dataFromArgs = {};
   for (let i = 0; i < args; i += 1) {
     let argument = args[i];
     ArgsOption.forEach((option) => {
       if (
-          argument.startsWith(`-${option.alias}`) ||
-          argument.startsWith(`--${option.name}`)
+        argument.startsWith(`-${option.alias}`) ||
+        argument.startsWith(`--${option.name}`)
       ) {
         const isAlias = argument.startsWith(`-${option.alias}`);
         const hasEqual = argument.includes('=');
         if (hasEqual) {
-
+          dataFromArgs[option.configOption] = isAlias
+            ? argument.replace(`-${option.alias}=`)
+            : argument.replace(`--${option.name}=`);
         }
       }
     });
   }
+  return dataFromArgs;
 };
