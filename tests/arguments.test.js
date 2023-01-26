0f0466b333a5ba121998ead1bd53e61e2776320c
chenp <chenpeleg@gmail.com>
2023-01-26 12:15:49 +0200

    another test file

---
+++ tests/arguments.test.js
@@ -0,0 +1,22 @@
+import {describe, it} from 'node:test';
+import {getConfigFromArgs} from '../src/config/getConfigFromArgs.js';
+import {ArgsOption} from '../src/config/argsOption.js';
+import assert from 'node:assert/strict';
+
+describe('Arguments ', () => {
+  it('ping', (_t) => {
+    const ping = () => 'pong';
+    assert.equal(ping(), 'pong');
+  });
+
+  it('argumantResult', (_t) => {
+    const argumantResult = getConfigFromArgs(
+        ' --email=my-email@gmail.com -mx=200 -mn=100'.split(' '),
+        ArgsOption
+    );
+
+    assert.equal(argumantResult.Email, 'my-email@gmail.com');
+    assert.equal(argumantResult.MaxDiffForSession, 200);
+    assert.equal(argumantResult.MinimumSessionMinuets, 100);
+  });
+});
--- tests/tests.js
+++ tests/tests.js
@@ -1,24 +1,9 @@
-import test from 'node:test';
+import {describe, it} from 'node:test';
 import assert from 'node:assert/strict';
-import { getConfigFromArgs } from '../src/config/getConfigFromArgs.js';
-import { ArgsOption } from '../src/config/argsOption.js';
 
-const ping = () => 'pong';
-
-test('ping', (_t) => {
-  const argumantResult = getConfigFromArgs(
-    '-email my-email@gmail.com',
-    ArgsOption
-  );
-  assert.equal(ping(), 'pong');
-});
-test('argumantResult', (_t) => {
-  const argumantResult = getConfigFromArgs(
-    ' --email=my-email@gmail.com -mx=200 -mn=100'.split(' '),
-    ArgsOption
-  );
-
-  assert.equal(argumantResult.Email, 'my-email@gmail.com');
-  assert.equal(argumantResult.MaxDiffForSession, 200);
-  assert.equal(argumantResult.MinimumSessionMinuets, 100);
+describe('First Tests', () => {
+  it('ping', (_t) => {
+    const ping = () => 'pong';
+    assert.equal(ping(), 'pong');
+  });
 });
