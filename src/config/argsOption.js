13e331cd03d1a151f1711b2a777715c3d38f509e
chenp <chenpeleg@gmail.com>
2023-01-26 10:32:25 +0200

    added command line args

--- src/config/argsOption.js
+++ src/config/argsOption.js
@@ -1,1 +1,48 @@
-export const ArgsOption = {}
+/**
+ *
+ * @type {CommandLineOption[]}
+ */
+export const ArgsOption = [
+  {
+    name: 'email',
+    alias: 'e',
+    type: 'string',
+    configOption: 'Email',
+  },
+  {
+    name: 'max-diff-for-session',
+    alias: 'mx',
+    type: 'number',
+    configOption: 'MaxDiffForSession',
+  },
+  {
+    name: 'min-session',
+    alias: 'mn',
+    type: 'number',
+    configOption: 'MinimumSessionMinuets',
+  },
+  {
+    name: 'date-from',
+    alias: 'df',
+    type: 'number',
+    configOption: 'DateFrom',
+  },
+  {
+    name: 'date-until',
+    alias: 'du',
+    type: 'number',
+    configOption: 'DateUntil',
+  },
+  {
+    name: 'output',
+    alias: 'o',
+    type: 'string',
+    configOption: 'output',
+  },
+  {
+    name: 'path',
+    alias: 'p',
+    type: 'string',
+    configOption: 'PathToRepo',
+  },
+];
--- src/types/reportConfigurations.d.ts
+++ src/types/reportConfigurations.d.ts
@@ -1,9 +1,16 @@
 export interface ReportConfigurations {
-    MaxDiffForSession: number; // maximum difference in minutes between commits counted to one session. Default: 120
-    MinimumSessionMinuets: number; // how many minutes first commit of session should add to total. Default: 120
-    DateFrom: Date; //Analyze data since certain date. [always|yesterday|tonight|lastweek|yyyy-mm-dd] Default: always'
-    DateUntil: Date; // Analyze data until certain date. [always|yesterday|today|lastweek|thisweek|yyyy-mm-dd] Default: today
-    Email: string; //  email address. Default: current git user email address
-    PathToRepo: string; //  Git repository to analyze. Default: .
-    output: 'csv' | 'console' | 'all';
+  MaxDiffForSession: number; // maximum difference in minutes between commits counted to one session. Default: 120
+  MinimumSessionMinuets: number; // how many minutes first commit of session should add to total. Default: 120
+  DateFrom: Date; //Analyze data since certain date. [always|yesterday|tonight|lastweek|yyyy-mm-dd] Default: always'
+  DateUntil: Date; // Analyze data until certain date. [always|yesterday|today|lastweek|thisweek|yyyy-mm-dd] Default: today
+  Email: string; //  email address. Default: current git user email address
+  PathToRepo: string; //  Git repository to analyze. Default: .
+  output: 'csv' | 'console' | 'all';
+}
+
+export interface CommandLineOption {
+  name: string;
+  alias: string;
+  type: 'boolean' | 'string' | 'number';
+  configOption?: keyof ReportConfigurations;
 }
