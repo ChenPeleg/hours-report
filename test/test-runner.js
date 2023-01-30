e8e5345a310f9e26d968cde29ae99f9d6e49f49c
chenp <chenpeleg@gmail.com>
2023-01-30 16:33:37 +0200

    added a stream promise

--- test/test-runner.js
+++ test/test-runner.js
@@ -1,3 +1,17 @@
 import { run } from 'node:test';
+import path from 'path';
 
-// run({}).pipe(process.stdout);
+const getStream = async () => {
+  const stream = run({ files: [path.resolve('./test/first.test.js')] }).pipe(
+    process.stdout
+  );
+
+  stream.on('end', (res) => Promise.resolve(res));
+};
+
+const testRunner = async () => {
+  const result = getStream();
+  console.log('************');
+  console.log(result);
+  console.log('************');
+};
