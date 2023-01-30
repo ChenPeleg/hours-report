66797f909836c226ec5f5e1479fae91099658924
chenp <chenpeleg@gmail.com>
2023-01-30 16:44:40 +0200

    failing test runner

--- test/test-runner.js
+++ test/test-runner.js
@@ -1,17 +1,25 @@
 import { run } from 'node:test';
 import path from 'path';
 
-const getStream = async () => {
-  const stream = run({ files: [path.resolve('./test/first.test.js')] }).pipe(
-    process.stdout
-  );
-
-  stream.on('end', (res) => Promise.resolve(res));
+const getStream = () => {
+  return new Promise((resolve, reject) => {
+    const tapStream = run({ files: [path.resolve('./test/first.test.js')] });
+    //     .pipe(
+    //   process.stdout
+    // );
+    tapStream.on('end', (res) => {
+      console.log('################');
+      resolve(tapStream);
+      console.log('################');
+    });
+  });
 };
 
 const testRunner = async () => {
-  const result = getStream();
   console.log('************');
+  const result = await getStream();
   console.log(result);
   console.log('************');
 };
+
+testRunner().then();
