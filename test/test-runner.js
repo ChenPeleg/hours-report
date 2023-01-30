e9e0ffc57a839d2b51f5cfbcf53b641a99a3931f
chenp <chenpeleg@gmail.com>
2023-01-30 18:05:29 +0200

    go all tap data as string

--- test/test-runner.js
+++ test/test-runner.js
@@ -1,8 +1,8 @@
-import { run } from 'node:test';
+import {run} from 'node:test';
 import path from 'path';
-import { getTestFiles } from './utils/getTestFiles.js';
+import {getTestFiles} from './utils/getTestFiles.js';
 
-const getTapDataAsync = async (testFiles) => {
+const getTapDataAsync = (testFiles) => {
   let allData = '';
   return new Promise((resolve, reject) => {
     const stream = run({
@@ -10,12 +10,13 @@ const getTapDataAsync = async (testFiles) => {
     });
     stream.on('data', (data) => (allData += data.toString()));
     stream.on('close', (data) => resolve(allData));
+    stream.on('error', (err) => reject(err));
   });
 };
 const mainRunner = async () => {
   const testFiles = (await getTestFiles(path.resolve('./test')))
-    .filter((f) => f.includes('test.js'))
-    .map((p) => path.resolve('./test', p));
+      .filter((f) => f.includes('test.js'))
+      .map((p) => path.resolve('./test', p));
   const result = await getTapDataAsync(testFiles);
   console.log(result);
 };
