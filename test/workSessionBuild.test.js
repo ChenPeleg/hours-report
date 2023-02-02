0f9212fe5d2f8b1cbd65b3f07fa950f1cfa6ae55
chenp <chenpeleg@gmail.com>
2023-02-02 10:49:25 +0200

    added console.log try and failed

--- test/workSessionBuild.test.js
+++ test/workSessionBuild.test.js
@@ -1,9 +1,10 @@
-import test, {describe, it} from 'node:test';
+import test, { describe, it } from 'node:test';
 import assert from 'node:assert/strict';
-import {WorkSessionsBuild} from '../src/core/workSessionsBuild.js';
+import { WorkSessionsBuild } from '../src/core/workSessionsBuild.js';
 
 describe('Work session build', () => {
+  console.log('asdfasdfasdf');
   it('should work', () => {
-
+    console.log('asdfasdfasdf');
   });
 });
