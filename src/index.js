2c90da7009fbd22c544c1d84eb1721fe4ffa7ab3
chenp <chenpeleg@gmail.com>
2023-02-02 09:51:02 +0200

    removed main file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-import { main } from './main.js';
+import { getGitLog } from './core/getGitLog.js';
+import { parseGitLogToEntries } from './core/parseGitLogToEntries.js';
+import { WorkSessionsBuild } from './core/workSessionsBuild.js';
+import { getConfiguration } from './config/getConfigurations.js';
+
+export const main = async () => {
+  try {
+    const config = getConfiguration(process.argv);
+    const gitLogData = await getGitLog();
+    const logEntries = parseGitLogToEntries(gitLogData.gitLog);
+    const buildWorkSessionsFromEntries = WorkSessionsBuild(logEntries, config);
+    return logEntries;
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 main().then((results) => results);
