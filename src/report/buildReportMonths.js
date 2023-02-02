3f908bbee08ded4fe1a6beede42a03bbfcf5b7a8
chenp <chenpeleg@gmail.com>
2023-02-02 14:13:08 +0200

    added build report months

---
+++ src/report/buildReportMonths.js
@@ -0,0 +1,9 @@
+/**
+ * @param {import("../types/Day.js").Day []} days
+ * @param {import("../types/reportConfigurations.js").ReportConfigurations} configuration
+ * @return {import("../types/Month.js").Month[] }
+ */
+
+export const buildReportMonths = (days, configuration) => {
+  return [];
+};
