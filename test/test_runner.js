63a9a63a9406a3f6e61ac77996e2066a01961df9
chenp <chenpeleg@gmail.com>
2023-02-02 13:16:44 +0200

    changed folder name to helpers

--- test/test_runner.js
+++ test/test_runner.js
@@ -1,15 +1,13 @@
-import {run} from 'node:test';
+import { run } from 'node:test';
 import path from 'path';
-import {getTestFiles} from './test-utils/getTestFiles.js';
-import {printTestResult} from './test-utils/printTestResult.js';
-
+import { getTestFiles } from './helpers/getTestFiles.js';
+import { printTestResult } from './helpers/printTestResult.js';
 
 /**
  * @param testFiles
  * @return {Promise<{data: string, pass: boolean}>}
  */
 const getTapDataAsync = (testFiles) => {
-
   let allData = '';
   let pass = true;
   return new Promise((resolve, reject) => {
@@ -18,15 +16,15 @@ const getTapDataAsync = (testFiles) => {
     });
     stream.on('data', (data) => (allData += data.toString()));
     stream.on('test:fail', (data) => (pass = false));
-    stream.on('close', (data) => resolve({data: allData, pass}));
+    stream.on('close', (data) => resolve({ data: allData, pass }));
     stream.on('error', (err) => reject(err));
   });
 };
 const mainRunner = async () => {
   try {
     const testFiles = (await getTestFiles(path.resolve('./test')))
-        .filter((f) => f.includes('test.js'))
-        .map((p) => path.resolve('./test', p));
+      .filter((f) => f.includes('test.js'))
+      .map((p) => path.resolve('./test', p));
     const result = await getTapDataAsync(testFiles);
     if (result) {
       printTestResult(result.data, result.pass);
