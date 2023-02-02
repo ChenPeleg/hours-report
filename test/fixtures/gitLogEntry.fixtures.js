d4480d841e656d3951ef8e160a2e18202799caf8
chenp <chenpeleg@gmail.com>
2023-02-02 11:01:32 +0200

    added git entry fixtures

--- test/fixtures/gitLogEntry.fixtures.js
+++ test/fixtures/gitLogEntry.fixtures.js
@@ -1,72 +1,154 @@
-export const gitLogsFixtures = {
-  fixture1:
-    '\n' +
-    '* 2023-01-30 21:54:05 +0200 cp@gmail.com uninstalled command line args\n' +
-    '* 2023-01-30 21:53:23 +0200 cp@gmail.com added parse fit log function\n' +
-    '* 2023-01-30 21:42:47 +0200 cp@gmail.com test should pass\n' +
-    '* 2023-01-30 21:42:35 +0200 cp@gmail.com test should faild\n' +
-    '* 2023-01-30 21:39:55 +0200 cp@gmail.com finished with the runner\n' +
-    '* 2023-01-30 21:33:26 +0200 cp@gmail.com added bolder colors\n' +
-    '* 2023-01-30 21:08:15 +0200 cp@gmail.com tests should fail\n' +
-    '* 2023-01-30 21:03:17 +0200 cp@gmail.com added green indication for passed tests\n' +
-    '* 2023-01-30 20:40:27 +0200 cp@gmail.com replaced test runner\n' +
-    '* 2023-01-30 20:38:30 +0200 cp@gmail.com changed the name runner\n' +
-    '* 2023-01-30 20:34:45 +0200 cp@gmail.com added nicer text 2\n' +
-    '* 2023-01-30 20:33:57 +0200 cp@gmail.com added nicer text\n' +
-    '* 2023-01-30 20:24:43 +0200 cp@gmail.com manipulating conclusions text\n' +
-    '* 2023-01-30 20:04:46 +0200 cp@gmail.com added consoleFormat.js\n' +
-    '* 2023-01-30 19:55:55 +0200 cp@gmail.com refactored for the test runner to return 0 on success 2\n' +
-    '* 2023-01-30 19:45:09 +0200 cp@gmail.com refactored for the test runner to return 0 on success\n' +
-    '* 2023-01-30 19:43:26 +0200 cp@gmail.com refactored to check if test failed 2\n' +
-    '* 2023-01-30 19:40:27 +0200 cp@gmail.com refactored to check if test failed\n' +
-    '* 2023-01-30 18:05:29 +0200 cp@gmail.com go all tap data as string\n' +
-    '* 2023-01-30 17:58:05 +0200 cp@gmail.com all tests passing\n' +
-    '* 2023-01-30 17:24:18 +0200 cp@gmail.com added test runner old\n' +
-    '* 2023-01-30 17:19:42 +0200 cp@gmail.com working diagnostics\n' +
-    '* 2023-01-30 16:59:37 +0200 cp@gmail.com all test pass\n' +
-    '* 2023-01-30 16:57:48 +0200 cp@gmail.com added more documentation\n' +
-    '* 2023-01-30 16:55:34 +0200 cp@gmail.com failing test runner 2\n' +
-    '* 2023-01-30 16:44:40 +0200 cp@gmail.com failing test runner\n' +
-    '* 2023-01-30 16:33:37 +0200 cp@gmail.com added a stream promise\n' +
-    '* 2023-01-30 16:09:48 +0200 cp@gmail.com all tests passing\n' +
-    '* 2023-01-30 13:32:30 +0200 cp@gmail.com added custom node test runner\n' +
-    '* 2023-01-30 12:57:11 +0200 cp@gmail.com fixed Line Imports \\n\n' +
-    '* 2023-01-30 12:36:48 +0200 cp@gmail.com this is \\n a multi line \\n\n' +
-    '* 2023-01-30 12:34:35 +0200 cp@gmail.com added gitlog entries\n' +
-    '* 2023-01-30 10:57:34 +0200 cp@gmail.com fixed missing node types\n' +
-    '* 2023-01-30 10:43:12 +0200 cp@gmail.com tested bad args names 2\n' +
-    '* 2023-01-26 17:06:35 +0200 cp@gmail.com tested bad args names\n' +
-    '* 2023-01-26 17:04:20 +0200 cp@gmail.com added more validation test\n' +
-    '* 2023-01-26 17:03:30 +0200 cp@gmail.com added more validation\n' +
-    '* 2023-01-26 16:48:40 +0200 cp@gmail.com throws if command is not legal\n' +
-    '* 2023-01-26 16:43:45 +0200 cp@gmail.com refactored arguments to get string\n' +
-    '* 2023-01-26 16:40:52 +0200 cp@gmail.com all tests passed\n' +
-    '* 2023-01-26 12:15:49 +0200 cp@gmail.com another test file\n' +
-    '* 2023-01-26 12:07:39 +0200 cp@gmail.com supporting numbers\n' +
-    '* 2023-01-26 12:03:29 +0200 cp@gmail.com tests failing\n' +
-    '* 2023-01-26 11:43:40 +0200 cp@gmail.com test not failing\n' +
-    '* 2023-01-26 11:43:23 +0200 cp@gmail.com finished with the buildConfigFromArgs.js\n' +
-    '* 2023-01-26 11:32:11 +0200 cp@gmail.com finished with the buildConfigFromArgs.js\n' +
-    '* 2023-01-26 11:24:29 +0200 cp@gmail.com added tests\n' +
-    '* 2023-01-26 11:21:42 +0200 cp@gmail.com testing works\n' +
-    '* 2023-01-26 11:10:21 +0200 cp@gmail.com refactored the function to buildConfigFromArgs.js\n' +
-    '* 2023-01-26 10:47:50 +0200 cp@gmail.com added command line args 4\n' +
-    '* 2023-01-26 10:42:12 +0200 cp@gmail.com added command line args 2\n' +
-    '* 2023-01-26 10:32:25 +0200 cp@gmail.com added command line args\n' +
-    '* 2023-01-26 10:22:30 +0200 cp@gmail.com added args parser\n' +
-    '* 2023-01-26 09:50:13 +0200 cp@gmail.com added reportConfigurations.d.ts\n' +
-    '* 2023-01-26 09:36:34 +0200 cp@gmail.com added inspiration\n' +
-    '* 2023-01-25 21:23:32 +0200 cp@gmail.com added promises\n' +
-    '* 2023-01-25 21:19:59 +0200 cp@gmail.com build gitlog function\n' +
-    '* 2023-01-25 21:06:22 +0200 cp@gmail.com extracted data to separate functions\n' +
-    '* 2023-01-25 20:57:34 +0200 cp@gmail.com success in getting the git log 2\n' +
-    '* 2023-01-25 20:57:26 +0200 cp@gmail.com success in getting the git log\n' +
-    '* 2023-01-25 20:53:49 +0200 cp@gmail.com added node commands\n' +
-    '* 2023-01-25 20:41:11 +0200 cp@gmail.com added tsconfig.json\n' +
-    '* 2023-01-25 19:28:34 +0200 cp@gmail.com configured prettier\n' +
-    '* 2023-01-25 19:27:25 +0200 cp@gmail.com added prettierrc.txt\n' +
-    '* 2023-01-25 19:22:00 +0200 cp@gmail.com added first test\n' +
-    '* 2023-01-25 19:19:02 +0200 cp@gmail.com added test\n' +
-    '* 2023-01-25 18:58:57 +0200 cp@gmail.com First commit\n' +
-    '* 2023-01-25 18:55:01 +0200 noreply@github.com Initial commit',
+export const gitEntryFixtures = {
+  fixture1: [
+    {
+      comment: 'uninstalled command line args',
+      date: '2023-01-30T19:54:05.000Z',
+      email: 'cp@gmail.com',
+    },
+    {
+      comment: 'added parse fit   log function',
+      date: '2023-01-30T19:53:23.000Z',
+      email: 'cp@gmail.com',
+    },
+    {
+      comment: 'test should pass',
+      date: '2023-01-30T19:42:47.000Z',
+      email: 'cp@g mail.com',
+    },
+    {
+      comment: 'test should faild',
+      date: '2023-01-30T19:42:35.000Z',
+      email: 'cp@gmail.com',
+    },
+    {
+      comment: 'finished with the runner',
+      date: '202 3-01-30T19:39:55.000Z',
+      email: 'cp@gmail.com',
+    },
+    {
+      comment: 'added bolder colors',
+      date: '2023-01-30T19:33:26.000Z',
+      email: 'cp@gmail.com',
+    },
+    {
+      comment: 'tests should fail',
+      date: '2023-01-30T19:08:15.000Z',
+      email: 'cp@gmail.com',
+    },
+    {
+      comment: 'added green indication for passed tests',
+      date: '2023-01-30 T19:03:17.000Z',
+      email: 'cp@gmail.com',
+    },
+    {
+      comment: 'replaced test runner',
+      date: '2023-01-30T18:40:27.000Z',
+      email: 'cp@gmail.com',
+    },
+    {
+      comment: 'chang ed the name runner',
+      date: '2023-01-30T18:38:30.000Z',
+      email: 'cp@gmail.com',
+    },
+    {
+      comment: 'added nicer text 2',
+      date: '2023-01-30T18:34:45.000Z',
+      'emai l': 'cp@gmail.com',
+    },
+    {
+      comment: 'added nicer text',
+      date: '2023-01-30T18:33:57.000Z',
+      email: 'cp@gmail.com',
+    },
+    {
+      comment: 'manipulating conclusions text',
+      date: '2023-01-30T18:24:43.000Z',
+      email: 'cp@gmail.com',
+    },
+    {
+      comment: 'added consoleFormat.js',
+      date: '2023-01-30T18:04:46.000Z',
+      email: 'cp@gmail.co m',
+    },
+    {
+      comment: 'refactored for the test runner to return 0 on success 2',
+      date: '2023-01-30T17:55:55.000Z',
+      email: 'cp@gmail.com',
+    },
+    {
+      comment: 'refact ored for the test runner to return 0 on success',
+      date: '2023-01-30T17:45:09.000Z',
+      email: 'cp@gmail.com',
+    },
+    {
+      comment: 'refactored to check if test fa iled 2',
+      date: '2023-01-30T17:43:26.000Z',
+      email: 'cp@gmail.com',
+    },
+    {
+      comment: 'refactored to check if test failed',
+      date: '2023-01-30T17:40:27.000Z',
+      ' email': 'cp@gmail.com',
+    },
+    {
+      comment: 'go all tap data as string',
+      date: '2023-01-30T16:05:29.000Z',
+      email: 'cp@gmail.com',
+    },
+    {
+      comment: 'all tests passing ',
+      date: '2023-01-30T15:58:05.000Z',
+      email: 'cp@gmail.com',
+    },
+    {
+      comment: 'added test runner old',
+      date: '2023-01-30T15:24:18.000Z',
+      email: 'cp@gmail.co m',
+    },
+    {
+      comment: 'working diagnostics',
+      date: '2023-01-30T15:19:42.000Z',
+      email: 'cp@gmail.com',
+    },
+    {
+      comment: 'all test pass',
+      date: '2023-01-30T14:59:37 .000Z',
+      email: 'cp@gmail.com',
+    },
+    {
+      comment: 'added more documentation',
+      date: '2023-01-30T14:57:48.000Z',
+      email: 'cp@gmail.com',
+    },
+    {
+      comment: 'failing te st runner 2',
+      date: '2023-01-30T14:55:34.000Z',
+      email: 'cp@gmail.com',
+    },
+    {
+      comment: 'failing test runner',
+      date: '2023-01-30T14:44:40.000Z',
+      email: 'cp@gmail.com',
+    },
+    {
+      comment: 'added a stream promise',
+      date: '2023-01-30T14:33:37.000Z',
+      email: 'cp@gmail.com',
+    },
+    {
+      comment: 'all tests passing',
+      date: '202 3-01-30T14:09:48.000Z',
+      email: 'cp@gmail.com',
+    },
+    {
+      comment: 'added custom node test runner',
+      date: '2023-01-30T11:32:30.000Z',
+      email: 'cp@gmail.com',
+    },
+    {
+      comment: 'fixed Line Imports     n',
+      date: '2023-01-30T10:57:11.000Z',
+      email: 'cp@gmail.com',
+    },
+  ],
 };
--- test/workSessionBuild.test.js
+++ test/workSessionBuild.test.js
@@ -1,12 +1,10 @@
-import test, { before, describe, it } from 'node:test';
+import test, {before, describe, it} from 'node:test';
 import assert from 'node:assert/strict';
-import { WorkSessionsBuild } from '../src/core/workSessionsBuild.js';
+import {WorkSessionsBuild} from '../src/core/workSessionsBuild.js';
 
 describe('Work session build', () => {
-  before(() => console.log('about to run some test'));
-  console.log('asdfasdfasdf');
   it('should work', () => {
-    console.log('asdfasdfasdf');
+
     assert.equal(1, 1);
   });
 });
