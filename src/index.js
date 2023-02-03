281cee8b8fc9f3169703f18e6ca98fdf257d0a3e
chenp <chenpeleg@gmail.com>
2023-02-03 23:58:00 +0200

    updated package

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { exportReport } from './export/exportReport.js';
 export const main = async () => {
   try {
     const { config, printHelp } = getConfiguration(process.argv);
-    if (printHelp || true) {
+    if (printHelp) {
       printHelpText();
       return;
     }
