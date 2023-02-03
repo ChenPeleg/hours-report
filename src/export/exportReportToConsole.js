e4985c82834c1f5a68beb8fb6316022a93fbed1b
chenp <chenpeleg@gmail.com>
2023-02-03 17:36:26 +0200

    added dir functionality

--- src/export/exportReportToConsole.js
+++ src/export/exportReportToConsole.js
@@ -1,6 +1,25 @@
-import { buildCsvAsString } from './buildCsvAsString.js';
-import { saveToCsvFile } from './saveToCsvFile.js';
-import { logToConsole } from '../utils/logToConsole.js';
+import {buildCsvAsString} from './buildCsvAsString.js';
+import {saveToCsvFile} from './saveToCsvFile.js';
+import {logToConsole} from '../utils/logToConsole.js';
+
+const STANDARD_CELL_SIZE = 10
+
+const makeCellsEvanSize = (csv) => {
+  const adjustCellsInRows = (row) => {
+    const cells = row.split(',')
+    const newCells = []
+    let passToNextCell = "";
+    for (let i = 0; i < cells.length; i++) {
+      let cell = cells[i]
+      if (cell.length > STANDARD_CELL_SIZE) {
+
+      }
+    }
+
+  }
+  const rows = csv.split('\n')
+
+}
 
 export const exportReportToConsole = (report) => {
   const csv = buildCsvAsString(report);
