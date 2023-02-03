777e1b7d23024d080cef3e9608fb19cd4676217c
chenp <chenpeleg@gmail.com>
2023-02-03 17:51:30 +0200

    added color to console

--- src/export/exportReportToConsole.js
+++ src/export/exportReportToConsole.js
@@ -1,27 +1,32 @@
-import {buildCsvAsString} from './buildCsvAsString.js';
-import {saveToCsvFile} from './saveToCsvFile.js';
-import {logToConsole} from '../utils/logToConsole.js';
+import { buildCsvAsString } from './buildCsvAsString.js';
+import { saveToCsvFile } from './saveToCsvFile.js';
+import { logToConsole } from '../utils/logToConsole.js';
 
-const STANDARD_CELL_SIZE = 10
+const STANDARD_CELL_SIZE = 9;
 
 const makeCellsEvanSize = (csv) => {
   const adjustCellsInRows = (row) => {
-    const cells = row.split(',')
-    const newCells = []
-    let passToNextCell = "";
+    const cells = row.split(',');
+    const newCells = [];
+    let passToNextCell = '';
     for (let i = 0; i < cells.length; i++) {
-      let cell = cells[i]
-      if (cell.length > STANDARD_CELL_SIZE) {
-
+      let cell = cells[i].replace(/\s/g, '');
+      if (cell.length < STANDARD_CELL_SIZE) {
+        cell += ' '.repeat(STANDARD_CELL_SIZE - cell.length);
       }
+      newCells.push(cell);
     }
-
-  }
-  const rows = csv.split('\n')
-
-}
+    //Details add color
+    return newCells.join('');
+  };
+  return csv
+    .split('\n')
+    .map((r) => adjustCellsInRows(r))
+    .join('\n');
+};
 
 export const exportReportToConsole = (report) => {
   const csv = buildCsvAsString(report);
-  logToConsole(csv);
+  const tableForConsole = makeCellsEvanSize(csv);
+  logToConsole(tableForConsole);
 };
