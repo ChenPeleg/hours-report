60b98bdefe7d6a27901674ddd9f24e6a783a7d25
chenp <chenpeleg@gmail.com>
2023-02-03 15:07:31 +0200

    only pushes non empty days and months

--- src/report/buildReportDays.js
+++ src/report/buildReportDays.js
@@ -1,27 +1,27 @@
-import { DateAndTimeUtil } from '../utils/dateAndTime.js';
+import {DateAndTimeUtil} from '../utils/dateAndTime.js';
 
 /**
  * @param {import("../types/Day.js").Day } day
  * @return {import("../types/Day.js").Day   }
  */
 const buildDayData = (day) => {
-  const dayWithData = { ...day };
+  const dayWithData = {...day};
   dayWithData.dayDate = day.workSessions[0].startTime;
   dayWithData.minuetSum = day.workSessions
-    .map((session) =>
-      DateAndTimeUtil.getMinutesBetweenDates(
-        session.startTime,
-        session.finishTime
+      .map((session) =>
+          DateAndTimeUtil.getMinutesBetweenDates(
+              session.startTime,
+              session.finishTime
+          )
       )
-    )
-    .reduce((a, b) => a + b, 0);
+      .reduce((a, b) => a + b, 0);
   const dayBranches = new Set();
   day.workSessions.forEach((s) => {
     s.branches.forEach((b) => dayBranches.add(b.trim()));
   });
   dayWithData.comments = Array.from(dayBranches)
-    .filter((b) => b.trim())
-    .join('; ');
+      .filter((b) => b.trim())
+      .join('; ');
   dayWithData.comments = dayWithData.comments.replace(/_/g, ' ').trim();
 
   return dayWithData;
@@ -43,25 +43,28 @@ export const buildDaysFromSessions = (workSessions, configuration) => {
   /** @type {import("../types/Day.js").Day [] } */
   let allDays = [];
   /** @type {import("../types/Day.js").Day   } */
-  let CurrentDay = { ...EmptyDay, workSessions: [] };
+  let CurrentDay = {...EmptyDay, workSessions: []};
   /** @type {import("../types/workSession.js").WorkSession } */
   let lastSession;
   for (let session of workSessions) {
     if (
-      !lastSession ||
-      DateAndTimeUtil.datesAreOnSameDay(
-        lastSession?.startTime,
-        session.startTime
-      )
+        !lastSession ||
+        DateAndTimeUtil.datesAreOnSameDay(
+            lastSession?.startTime,
+            session.startTime
+        )
     ) {
       CurrentDay.workSessions.push(session);
     } else if (lastSession) {
       allDays.push(CurrentDay);
-      CurrentDay = { ...EmptyDay, workSessions: [] };
+      CurrentDay = {...EmptyDay, workSessions: []};
       CurrentDay.workSessions.push(session);
     }
     lastSession = session;
   }
-  allDays.push(CurrentDay);
+  if (CurrentDay.workSessions.length) {
+    allDays.push(CurrentDay);
+  }
+
   return allDays.map((d) => buildDayData(d));
 };
