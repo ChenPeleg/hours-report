208514f9cd42b10d130ed6a92638298290ac9e56
chenp <chenpeleg@gmail.com>
2023-02-03 15:02:24 +0200

    fixing days bug

--- test/reportBuildMonths.test.js
+++ test/reportBuildMonths.test.js
@@ -5,6 +5,8 @@ import { defaultConfig } from '../src/config/defaultConfig.js';
 import assert from 'node:assert';
 import { dayFixture } from './fixtures/dayFixture.js';
 import { buildReportMonths } from '../src/report/buildReportMonths.js';
+import { buildReportFromSession } from '../src/report/buildReport.js';
+import { DateAndTimeUtil } from '../src/utils/dateAndTime.js';
 
 describe('Build report', () => {
   it('Builds months from days correctly', () => {
@@ -44,5 +46,42 @@ describe('Build report', () => {
       new Set(jsDays.map((d) => d.dayDate.getMonth()))
     );
     assert.equal(months.length, numberOfMonthsInDays.length);
+  });
+  it('Builds Report from sessions correctly', () => {
+    const fixture = workSessionFixtures.fixture1;
+    /**
+     * @type import('../src/types/workSession.js').WorkSession[]
+     */
+    const sessions = fixture.map((s) => {
+      /**
+       * @type import('../src/types/workSession.js').WorkSession
+       */
+      const dateSession = {
+        ...s,
+        startTime: new Date(s.startTime),
+        finishTime: new Date(s.finishTime),
+        logEntries: [],
+      };
+      dateSession.logEntries = s.logEntries.map((le) => ({
+        ...le,
+        date: new Date(le.date),
+        email: le.email,
+      }));
+      return dateSession;
+    });
+
+    const report = buildReportFromSession(sessions, defaultConfig, 'repo-name');
+    const minuetsFromSessions = sessions
+      .map((s) =>
+        DateAndTimeUtil.getMinutesBetweenDates(s.startTime, s.finishTime)
+      )
+      .reduce((a, b) => a + b);
+    assert.equal(report.repoName, 'repo-name');
+    // assert.equal(
+    //   report.months.map((m) => m.days.map((d) => d.dayDate)),
+    //   {}
+    // );
+    // assert.equal(sessions[0], {});
+    assert.equal(report.minuetSum, minuetsFromSessions);
   });
 });
