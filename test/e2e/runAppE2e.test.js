1f87bd4e7cfed8759ecd711254763ab0c698a228
chenp <chenpeleg@gmail.com>
2023-02-04 23:38:35 +0200

    added accessibility 2

--- test/e2e/runAppE2e.test.js
+++ test/e2e/runAppE2e.test.js
@@ -1,23 +1,24 @@
 import assert from 'assert';
-import { readFileSync } from 'fs';
-import { describe, it } from 'node:test';
-import { execPromise } from '../../src/utils/execPromise.js';
+import {readFileSync} from 'fs';
+import {describe, it} from 'node:test';
+import {execPromise} from '../../src/utils/execPromise.js';
 
 const outputFolder = 'output';
 
 describe('Run app e2e', async () => {
-  it('run app on git repo - accessibility', async () => {
-    const res = await execPromise(
-      `npm run start -- -o=./${outputFolder} -p=../accessibility -e=chenpeleg@gmail.com`
-    );
-    const resLines = res.split('\n').slice(2);
-    const outputLine = resLines.find((l) => l.includes(outputFolder));
+    it('run app on git repo - accessibility', async () => {
+        const exceResopnse = await execPromise(
+            `npm run start -- -o=./${outputFolder} -p=../accessibility -e=chenpeleg@gmail.com`
+        );
+        assert.equal(exceResopnse, '');
+        const resLines = exceResopnse.split('\n').slice(2);
+        const outputLine = resLines.find((l) => l.includes(outputFolder));
 
-    const hash = outputLine.split(outputFolder + '/')[1];
-    const pathToFile = `./${outputFolder}/hours-report-${hash}.csv`;
-    const resultCsv = readFileSync(pathToFile);
-    assert.equal(resultCsv.length > 5000, true);
-    assert.equal(resultCsv.includes('Total hours'), true);
-    assert.equal(resultCsv.includes('Total hours,,11'), true);
-  });
+        const hash = outputLine.split(outputFolder + '/')[1];
+        const pathToFile = `./${outputFolder}/hours-report-${hash}.csv`;
+        const resultCsv = readFileSync(pathToFile);
+        assert.equal(resultCsv.length > 5000, true);
+        assert.equal(resultCsv.includes('Total hours'), true);
+        assert.equal(resultCsv.includes('Total hours,,11'), true);
+    });
 });
