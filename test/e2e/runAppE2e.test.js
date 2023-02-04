935e68db6ff42cde276e06af9917ec2d1e2e2e55
chenp <chenpeleg@gmail.com>
2023-02-04 23:51:14 +0200

    added readFileSync to string

--- test/e2e/runAppE2e.test.js
+++ test/e2e/runAppE2e.test.js
@@ -1,6 +1,7 @@
 import assert from 'assert';
 import { readFileSync } from 'fs';
 import { describe, it } from 'node:test';
+import path from 'path';
 import { execPromise } from '../../src/utils/execPromise.js';
 
 const outputFolder = 'output';
@@ -15,7 +16,8 @@ describe('Run app e2e', async () => {
 
     const hash = outputLine.split(outputFolder + '/')[1];
     const pathToFile = `./${outputFolder}/hours-report-${hash}.csv`;
-    const resultCsv = readFileSync(pathToFile);
+    const resultCsv = readFileSync(path.resolve(pathToFile)).toString();
+ 
     assert.equal(resultCsv.length > 5000, true);
     assert.equal(resultCsv.includes('Total hours'), true);
     assert.equal(resultCsv.includes('Total hours,,11'), true);
