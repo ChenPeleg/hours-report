e68d5f49daa0cdf4b190b6b365db536f12aa311d
chenp <chenpeleg@gmail.com>
2023-05-04 19:12:39 +0300

    format files

--- src/xlsx/types/worksheet.types.ts
+++ src/xlsx/types/worksheet.types.ts
@@ -1,36 +1,40 @@
-
-
-export type CellBorder = 'top' | 'bottom' | 'right' | 'left' | 'all' | {
-  position: 'top' | 'bottom' | 'right' | 'left' | 'all';
-  width?:  "thin" | "medium" | "thick" | "dotted" | "hair" | "double";
-  color? : string
-}
+export type CellBorder =
+  | "top"
+  | "bottom"
+  | "right"
+  | "left"
+  | "all"
+  | {
+      position: "top" | "bottom" | "right" | "left" | "all";
+      width?: "thin" | "medium" | "thick" | "dotted" | "hair" | "double";
+      color?: string;
+    };
 export interface CellStyle {
-    background?: string; 
-    font? : {
-      size?: number;
-      bold?: boolean;
-      color? : string;  
-    } 
-    border?: CellBorder[];
-    styleId?: string;
-} 
+  background?: string;
+  font?: {
+    size?: number;
+    bold?: boolean;
+    color?: string;
+  };
+  border?: CellBorder[];
+  styleId?: string;
+}
 export interface Cell {
-    dataType?: "number" | "string" | "empty";
-    value: number | string;
-    style?: CellStyle;
+  dataType?: "number" | "string" | "empty";
+  value: number | string;
+  style?: CellStyle;
 }
 export interface Row {
   cells: Cell[];
-  height? : number
+  height?: number;
 }
 
 export interface Sheet {
   rows: Row[];
   name: string;
-  columnWidth?: number[] 
+  columnWidth?: number[];
 }
 export interface Workbook {
-    name : string;
-    sheets : Sheet [];
+  name: string;
+  sheets: Sheet[];
 }
