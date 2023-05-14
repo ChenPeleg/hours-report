86e929cdee8ceb48d1a5a62788a52c0772dc01c4
chenp <chenpeleg@gmail.com>
2023-05-14 09:45:56 +0300

    added full report option

--- src/types/reportConfigurations.d.ts
+++ src/types/reportConfigurations.d.ts
@@ -7,6 +7,7 @@ export interface ReportConfigurations {
   PathToRepo: string; //  Git repository to analyze. Default: .
   outputFormat: "csv" | "console" | "all";
   outputFolder: string;
+  fullReport: boolean;
 }
 
 export interface CommandLineOption {
